7b003a0c9ee0ec9d55706be1e7c4061a5f2b5477
Njiraini23 <njiraini88@gmail.com>
2024-10-28 17:00:56 +0300

    Creating the API calls for the products loading

--- src/pages/react-query/index.jsx
+++ src/pages/react-query/index.jsx
@@ -1,9 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchListOfProducts } from "./api";
 
 
 function ReactQuerry(){
+
+    const {data: productList, isLoading} = useQuery({
+        queryKey : ['productList'],
+        queryFn : ()=> fetchListOfProducts()
+    });
+
+    if(isLoading) return <h1>Loading products ! Please wait</h1>
+
+    
+
     return (
         <div>
             <h1>React Query Demo</h1>
+            <ul>
+                {
+                    productList?.length > 0 ? (
+                    productList.map((product)=> (
+                    <li key={product.id} >{product.title} </li> 
+                ))
+                 ) : (
+                     <h3>No Product Found</h3>
+                )}
+            </ul>
         </div>
     )
 }
